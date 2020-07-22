5d33af31472dfb2469f5071cce5f34b226a8f557
Gerd Reiss <gerd@reiss.pro>
2020-07-22 08:26:47 +0200

    finer modularization

--- src/Run.hs
+++ src/Run.hs
@@ -8,10 +8,10 @@ where
 
 import qualified Data.ByteString.Lazy          as B
 import qualified Data.ByteString.Lazy.Char8    as C8
+import           Git
+import           Logging
 import           RIO
-import           RIO.Process
-import           System.Directory               ( doesDirectoryExist
-                                                , listDirectory
+import           System.Directory               ( listDirectory
                                                 , makeAbsolute
                                                 , setCurrentDirectory
                                                 )
@@ -20,27 +20,17 @@ import           Types
 
 run :: RIO App ()
 run = do
-  root   <- view directoryL
-  master <- view masterL
-  logInput master root
-  listRepos >>= updateRepos master
+  root      <- view directoryL
+  recursive <- view recursiveL
+  master    <- view masterL
+  logInput recursive master root
+  listRepos recursive root >>= updateRepos master
 
-resolveRoot :: String -> String
-resolveRoot root | root == "."  = "current directory"
-                 | root == ".." = "parent directory"
-                 | root == "~"  = "home directory"
-                 | otherwise    = root
-
-listRepos :: RIO App [FilePath]
-listRepos = do
-  root <- view directoryL
-  liftIO $ do
-    path <- makeAbsolute root
-    dirs <- map (path </>) <$> listDirectory path
-    filterM isGitRepo dirs
-
-isGitRepo :: FilePath -> IO Bool
-isGitRepo dir = doesDirectoryExist $ dir </> ".git"
+listRepos :: Bool -> FilePath -> RIO App [FilePath]
+listRepos recursive root = liftIO $ do
+  path <- makeAbsolute root
+  dirs <- map (path </>) <$> listDirectory path
+  filterM isGitRepo dirs
 
 updateRepos :: Bool -> [FilePath] -> RIO App ()
 updateRepos master = mapM_ (updateRepo master)
@@ -48,7 +38,7 @@ updateRepos master = mapM_ (updateRepo master)
 updateRepo :: Bool -> FilePath -> RIO App ()
 updateRepo master repo = do
   logRepo repo
-  liftIO $ setCurrentDirectory repo
+  liftIO (setCurrentDirectory repo)
   gitPull
   when master updateMasterBranch
 
@@ -56,41 +46,17 @@ updateMasterBranch :: RIO App ()
 updateMasterBranch = gitBranch >>= processBranch
 
 processBranch :: ReadProcessResult -> RIO App ()
-processBranch (ExitSuccess, out, _) = do
-  unless (isMasterBranch out) $ do
-    logInfo . fromString $ "Checkout and update master branch"
-    gitCheckoutMaster
-    gitPull
+processBranch (ExitSuccess, out, _) = unless (isMasterBranch out) $ do
+  logInfo . fromString $ "Checkout and update master branch"
+  gitCheckoutMaster
+  gitPull
 processBranch (ExitFailure code, _, err) =
   logError
     . fromString
     . concat
-    $ ["Failed listing branches with code", show code, " and error ", show err]
-
-gitBranch :: RIO App ReadProcessResult
-gitBranch = proc "git" ["branch"] readProcess
-
-gitPull :: RIO App ()
-gitPull = proc "git" ["pull"] runProcess_
-
-gitCheckoutMaster :: RIO App ()
-gitCheckoutMaster = proc "git" ["checkout", "master"] runProcess_
+    $ ["Failed listing branches with code ", show code, " and error ", show err]
 
 isMasterBranch :: B.ByteString -> Bool
 isMasterBranch s = "* master" `elem` branches
   where branches = lines . C8.unpack $ s
 
-logInput :: Bool -> FilePath -> RIO App ()
-logInput master root =
-  logInfo
-    . fromString
-    . concat
-    $ [ "Updating "
-      , if master then "master branches of the " else " "
-      , "GIT repos in "
-      , resolveRoot root
-      , "..."
-      ]
-
-logRepo :: FilePath -> RIO App ()
-logRepo repo = logInfo . fromString $ "updating repo: " <> repo
--- src/Types.hs
+++ src/Types.hs
@@ -8,7 +8,6 @@ import           RIO.Process
 
 type ReadProcessResult = (ExitCode, B.ByteString, B.ByteString)
 
--- | Command line arguments
 data Options =
   Options
     { optionsDirectory :: !FilePath
@@ -27,6 +26,9 @@ data App =
 class HasDirectory env where
   directoryL :: Lens' env FilePath
 
+class HasRecursive env where
+  recursiveL :: Lens' env Bool
+
 class HasMaster env where
   masterL :: Lens' env Bool
 
@@ -36,6 +38,13 @@ instance HasDirectory App where
     appOptionsL = lens appOptions (\x y -> x { appOptions = y })
     optionsDirectoryL =
       lens optionsDirectory (\x y -> x { optionsDirectory = y })
+
+instance HasRecursive App where
+  recursiveL = appOptionsL . optionsRecursiveL
+   where
+    appOptionsL = lens appOptions (\x y -> x { appOptions = y })
+    optionsRecursiveL =
+      lens optionsRecursive (\x y -> x { optionsRecursive = y })
 
 instance HasMaster App where
   masterL = appOptionsL . optionsMasterL
